6deb408f39e3c355679daf48bb179f247c9fc3de
Zaland <sanikhankhan749@gmail.com>
2025-07-30 05:46:53 -0700

    added the functionality for the delete and delete all buttons and styled the page

--- AI-Prompt-Collector/src/App.jsx
+++ AI-Prompt-Collector/src/App.jsx
@@ -1,33 +1,75 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css'
 
 function App() {
-  const [inputText, setInputText] = useState('');
-  const [prompts, setPrompts] = useState([]);
+  //const fromLocalStorage = JSON.parse(localStorage.getItem('prompt'));
 
-  function save(promptText) {
-    setPrompts([...prompts, promptText]);
-  }
+  const [inputText, setInputText] = useState('');
+  const [prompts, setPrompts] = useState(JSON.parse(localStorage.getItem('prompts')) || []);
 
   function saveInputText(event) {
     setInputText(event.target.value);
   }
 
+  function save(promptText) {
+    setPrompts([...prompts, promptText]);
+    setInputText('');
+  }
+
+  function inputSaveByEnter(event) {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      save(inputText)
+    }
+  }
+
+  function deletePromptBtn(index) {
+    setPrompts(prompts.filter((_, i) => i !== index));
+  }
+
+  function deleteAll() {
+    setPrompts([]);
+  }
+
+  useEffect(() => {
+    localStorage.setItem('prompts', JSON.stringify(prompts));
+  }, [prompts]);
+
   return (
-    <div>
-      <input type='text' placeholder='Write a Prompt' onChange={saveInputText} />
-      <button onClick={() => save(inputText)}>Save</button>
+    <div className='container'>
+      <input
+        type='text'
+        placeholder='Write a Prompt'
+        onChange={saveInputText}
+        value={inputText}
+        onKeyDown={inputSaveByEnter}
+        className='input' />
 
-      <div>
-        {
-          prompts.map((prompt, index) => {
-            return (
-              <div key={index}>{prompt}</div>
-            );
-          })
-        }
-      </div>
+      <button
+        onClick={() => save(inputText)}
+        className='save-button'>Save</button>
 
+      <button
+        onClick={deleteAll}
+        className='delete-all-button'>Delete All</button>
+
+      {
+        prompts.map((prompt, index) => {
+          return (
+            <div
+              key={index}
+              className='prompts-container'>
+                
+              <span className='prompt-content'>{prompt}</span>
+
+              <button
+                onClick={() => { deletePromptBtn(index)}}
+                className='delete-button'
+              >Delete</button>
+            </div>
+          );
+        })
+      }
     </div>
   );
 }
