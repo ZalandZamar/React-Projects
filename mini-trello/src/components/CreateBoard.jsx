6651ab87442ae8884ca0ace7609493b9d11ee2ed
Zaland <sanikhankhan749@gmail.com>
2025-09-01 23:07:00 -0700

    created the CReateBoard component and set up the project

---
+++ mini-trello/src/components/CreateBoard.jsx
@@ -0,0 +1,9 @@
+import '../css/CreateBoard.css'
+
+export const CreateBoard = () => {
+  return(
+    <div>
+      <button className="create-board-button">+&nbsp;&nbsp;Create Board</button>
+    </div>
+  )
+}
